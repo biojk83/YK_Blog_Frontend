a78e909d9991e929d08a31b9a1d56d1b53f224ed
Youngwok Kim <youngwok.kim83@gmail.com>
2022-12-02 16:13:39 -0500

    trying exact on about contact

--- src/App.js
+++ src/App.js
@@ -31,8 +31,8 @@ function App() {
       <Route path="/write" element={user ? <Write /> : <Login />} />
       <Route path="/settings" element={user ? <Setting /> : <Login />} />
       <Route path="/post/:postId" element={<Single />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
+      <Route exact path="/about" element={<About />} />
+      <Route exact path="/contact" element={<Contact />} />
       </Routes>
     </Router>    
   );
